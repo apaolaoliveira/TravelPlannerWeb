95c6f532311c77888d1f20715d4a960ae80b6d55
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-12 16:06:48 -0300

    add confirm trip modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react';
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User } from 'lucide-react';
 import { FormEvent, useState } from 'react';
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
   const [emailsToInvite, setEmailsToInvite] = useState([
     'apaolaoliveira@gmail.com',
   ]);
@@ -40,6 +41,14 @@ export function App() {
   function removeEmailFromInvites(emailToRemove: string){
     const newEmailList = emailsToInvite.filter(email => email !== emailToRemove);
     setEmailsToInvite(newEmailList);
+  }
+
+  function openConfirmTripModal(){
+    setIsConfirmTripModalOpen(true);
+  }
+
+  function closeConfirmTripModal(){
+    setIsConfirmTripModalOpen(false);
   }
 
   return (
@@ -82,12 +91,16 @@ export function App() {
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
               <button onClick={openGuestsModal} className="flex items-center gap-2 flex-1">
                 <UserRoundPlus className="size-5 text-zinc-400"/>
-                <span className="text-zinc-400 text-lg flex-1 text-left">Who will be on the trip?</span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-zinc-100 text-lg flex-1 text-left">{emailsToInvite.length} invited guests</span>
+                ):(
+                  <span className="text-zinc-400 text-lg flex-1 text-left">Who will be on the trip?</span>
+                )}
               </button>
 
               <div className="w-px h-6 bg-zinc-800"></div>
 
-              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Confirm Trip
                 <ArrowRight className="size-5 "/>
               </button>
@@ -146,6 +159,50 @@ export function App() {
                 <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                   Invite
                   <Plus className="size-5 "/>
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+
+        { isConfirmTripModalOpen && (
+          <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <h2 className="text-lg font-semibold">Confirm booking details</h2>
+                  <button type="button" onClick={closeConfirmTripModal} className="">
+                    <X className="size-5 text-zinc-400"/>
+                  </button>
+                </div>
+                <p className="text-sm text-zinc-400 text-left">
+                  Please enter your information below to finalize your trip to 
+                  <span className="font-semibold text-zinc-100"> Florianópolis, Brazil </span> 
+                  scheduled for <span className="font-semibold text-zinc-100"> August 16 to 27, 2024.</span>
+                </p>
+              </div>
+
+              <form onSubmit={addNewEmailToInvite} className="space-y-3">
+                <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <User className="text-zinc-400 size-5"/>
+                  <input 
+                    type="text" 
+                    name="name"
+                    placeholder="Full name" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+                <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <AtSign className="text-zinc-400 size-5"/>
+                  <input 
+                    type="email" 
+                    name="email"
+                    placeholder="Personal email" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+                <button type="submit" className="w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Confirm trip
                 </button>
               </form>
             </div>
