3771a988dcfe30831946b5823efcc8565eb043e7
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-07-20 23:53:08 -0300

    add activities section

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Plus, Settings2 } from 'lucide-react';
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react';
 
 export function TripDetailsPage(){
   return (
@@ -24,15 +24,40 @@ export function TripDetailsPage(){
         </div>
       </div>
 
-      <main className="flex gap-16">
+      <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
-            <h2 className='text-3xl font-semibold'>Atividade</h2>
+            <h2 className='text-3xl font-semibold'>Activity</h2>
             <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <Plus className="size-5"/>
-              Cadastrar atividade
+              New activity
             </button>
           </div>
+
+          <section className="space-y-8">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl font-semibold text-zinc-300">Dia 17</span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p className="text-sm text-zinc-500">No record activities for this date</p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl font-semibold text-zinc-300">Dia 18</span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300"/>
+                  <span className="text-zinc-100">Group activity</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
 
         <div className="w-80"></div>
