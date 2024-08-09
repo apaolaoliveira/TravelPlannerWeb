cdf190e7e437c8d421b2ac9c7537a5cd9d10374a
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-08-09 19:52:52 -0300

    upd var env

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   MODE: z.enum(['production', 'development', 'test']),
-  REACT_APP_API_URL: z.string(),
+  VITE_APP_API_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { env } from "../env";
 
-console.log(`Url ${env.REACT_APP_API_URL}`);
+console.log(`Url ${env.VITE_APP_API_URL}`);
 
 export const api = axios.create({
-  baseURL: env.REACT_APP_API_URL
+  baseURL: env.VITE_APP_API_URL
 });
