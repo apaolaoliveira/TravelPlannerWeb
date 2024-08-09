83754adfa0db243da42d8efd4d076ad80c495a97
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-08-09 19:21:28 -0300

    debug env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log(`Url ${process.env.REACT_APP_API_URL}`);
+
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL
 });
